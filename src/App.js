de2c3ac132e38ccc66c2a65d973f96168044a548
ThakareSakshi <sakshithakare121@gmail.com>
2024-01-04 16:16:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+import Question from './components/Question';
 
 function App() {
+
+  const [data,setData]=useState([]);
+  const apiKey = 'I34YZGfJuiwmithX63h0hPmLvEj07a3CRwzuPT7A';
+  const apiUrl = 'https://quizapi.io/api/v1/questions';
+  
+  useEffect(()=>{
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl, {
+          params: {
+            apiKey: apiKey,
+            limit: 10, // Adjust the limit as needed
+          },
+        });
+        // console.log(response.data)
+        setData(response.data)
+        // setData(response.data);
+      } catch (error) {
+        console.log('Error fetching quiz questions:', error);
+      }
+    };
+
+    fetchData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Question data={data}/>
+      
     </div>
   );
 }
---
+++ src/components/Question.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {useState} from 'react'
+
+const Question = ({data}) => {
+
+    const [questionNumber,setquestionNumber]=useState(0);
+    const [score,setScore]=useState(0);
+    // console.log(data)
+
+    const handleQuestions=(e)=>{
+      console.log(e.target.textContent);
+      console.log(data[questionNumber].answers[data[questionNumber].correct_answer])
+        if(e.target.textContent==data[questionNumber].answers[data[questionNumber].correct_answer]){
+            setScore(score+1);
+        }
+       setquestionNumber(questionNumber+1);
+       if(questionNumber<10){
+        console.log(score);
+       }
+    }
+    
+    // console.log(data)
+  return (
+    <div className='Question-set'>
+      
+      {
+        data.length!=0 && questionNumber<10?
+      
+      
+        <>
+            <h1>Questions No:{questionNumber+1} </h1>
+            <p>{data[questionNumber].question}</p>
+            <ul>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_a}</li>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_b}</li>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_c}</li>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_d}</li>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_e}</li>
+              <li onClick={handleQuestions}>{data[questionNumber].answers.answer_f}</li>
+            </ul> 
+            <br/>
+      <button onClick={handleQuestions}>skip question</button>
+        </>:
+        <div className='score'>
+          <h2>Quiz End</h2>
+          <p>Your Score</p>
+          <p>{score}</p>
+        </div>
+      
+      
+      
+      }
+     
+       
+      
+    </div>
+  )
+}
+
+export default Question
